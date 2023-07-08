962d42a76fb4124ec52177cc59f91a8155c50848
Berndt Uhlig <berndt.uhlig@libertari.at>
2023-07-08 22:57:56 +0200

    [C1] Initial commit.

---
+++ src/main/java/com/example/craftworkschallenge/CraftworksChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.craftworkschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftworksChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CraftworksChallengeApplication.class, args);
+    }
+
+}
